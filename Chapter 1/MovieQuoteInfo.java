c1abb2af858dae8c16853ae18522fd433838ca68
KeanCaballero <keancaballero147@gmail.com>
2024-08-20 00:09:04 +0800

    Update and rename Exercise1.txt to MovieQuoteInfo.java

--- Chapter 1/MovieQuoteInfo.java
+++ Chapter 1/MovieQuoteInfo.java
@@ -3,7 +3,7 @@ class MovieQuoteInfo
    public static void main(String[] args)
    {
       System.out.println("Be brave and upright, that God may love thee. Speak the truth always, even if it leads to your death. Safeguard the helpless and do no wrong; that is your oath.");
-      System.out.println("Movie:King Baldwin IV, Kingdom of Heaven.");
+      System.out.println("Movie:Kingdom of Heavens, King Baldwin IV.");
       System.out.println("Actor:Edward Norton");
       System.out.println("Year: 2005");
    }
