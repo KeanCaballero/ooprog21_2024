db5f41d07e2b54d27a4d2eca59e96f9551f14d77
KeanCaballero <keancaballero147@gmail.com>
2024-09-16 16:34:32 +0800

    Create FirstAddressExcercise.java

---
+++ Chapter 3 /FirstAddressExcercise.java
@@ -0,0 +1,13 @@
+public class FirstAddressExercise {
+
+    public static void main(String[] args){
+    displayAddress();
+    }
+
+    public static void displayAddress() {
+    System.out.println("Xiaomi Corporation");
+    System.out.println("68 Qinghe Middle Street, Haidian District");
+    System.out.println("Beijing, China 100085");
+    System.out.println("Company: Xiaomi Corporation");
+    }
+}
